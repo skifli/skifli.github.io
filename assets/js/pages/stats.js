befa96b1608803f97f5e2c9d1fc417767be93754
skifli <121291719+skifli@users.noreply.github.com>
2023-03-08 08:22:28 +0000

    Add more stats to stats page

--- assets/js/pages/stats.js
+++ assets/js/pages/stats.js
@@ -5,18 +5,24 @@ window.addEventListener("load", async function () {
     this.document.getElementById("api-declaration").innerHTML = `Retreived stats from the <a href="https://docs.github.com/en/rest" class="link" target="_blank">GitHub API</a> in ${this.performance.now() - start} ms.`;
 
     statsJSON = JSON.parse(statsJSON); // parse stats into a json object
-    let statsData = { "stars": 0, "forks": 0, "watchers": 0 }; // instantiate the stats data
+    let statsData = { "repo_owns": 0, "open_issues_count": 0, "forks_count": 0, "stars_received": 0, "forks_received": 0, "watchers_received": 0 }; // instantiate the stats data
 
-    for (let i = 0; i < statsJSON.repos.length; i++) {
-        let repo = statsJSON.repos[i];
+    for (let i = 0; i < statsJSON["repos"].length; i++) {
+        let repo = statsJSON["repos"][i];
 
-        statsData["stars"] += repo["stargazers_count"];
-        statsData["forks"] += repo["forks_count"];
-        statsData["watchers"] += repo["watchers_count"];
+        statsData["repo_owns"] += (repo["owner"]["login"] == "skifli" ? 1 : 0);
+        statsData["open_issues_count"] += repo["open_issues_count"];
+        statsData["forks_count"] += (repo["fork"] ? 1 : 0);
+        statsData["stars_received"] += repo["stargazers_count"];
+        statsData["forks_received"] += repo["forks_count"];
+        statsData["watchers_received"] += repo["watchers_count"];
     }
 
-    this.document.getElementById("repo-contribs").innerHTML = `Contributed to ${statsJSON.repos.length} repositor${statsJSON.repos.length == 1 ? "y" : 'ies'}.`;
-    this.document.getElementById("stars-count").innerHTML = `Received ${statsData["stars"]} star${statsData["stars"] == 1 ? "" : "s"}.`;
-    this.document.getElementById("forks-count").innerHTML = `Received ${statsData["forks"]} fork${statsData["forks"] == 1 ? "" : "s"}.`;
-    this.document.getElementById("watchers-count").innerHTML = `Received ${statsData["watchers"]} watcher${statsData["watchers"] == 1 ? "" : "s"}.`;
+    this.document.getElementById("repo-contribs").innerHTML = `I have contributed to ${statsJSON["repos"].length} repositor${statsJSON["repos"].length == 1 ? "y" : "ies"}.`;
+    this.document.getElementById("repo-owns").innerHTML = `I own ${statsData["repo_owns"]} repositor${statsData["repo_owns"] == 1 ? "y" : "ies"}.`;
+    this.document.getElementById("open-issues-count").innerHTML = `I have ${statsData["open_issues_count"]} unresolved issue${statsData["open_issues_count"] == 1 ? "" : "s"} in my repositor${statsJSON["repos"].length == 1 ? "y" : "ies"}.`;
+    this.document.getElementById("forks-count").innerHTML = `I have forked ${statsData["forks_count"]} repositor${statsData["forks_count"] == 1 ? "y" : "ies"}.`;
+    this.document.getElementById("stars-received").innerHTML = `I have received ${statsData["stars_received"]} star${statsData["stars_received"] == 1 ? "" : "s"}.`;
+    this.document.getElementById("forks-received").innerHTML = `I have received ${statsData["forks_received"]} fork${statsData["forks_received"] == 1 ? "" : "s"}.`;
+    this.document.getElementById("watchers-received").innerHTML = `I have received ${statsData["watchers_received"]} watcher${statsData["watchers_received"] == 1 ? "" : "s"}.`;
 })
