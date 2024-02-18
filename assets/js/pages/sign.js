ce1880283da5c9c9eda8bc7816bcd05795ac5fa3
skifli <121291719+skifli@users.noreply.github.com>
2024-02-18 18:20:19 +0000

    fix: signing failing?

--- assets/js/pages/sign.js
+++ assets/js/pages/sign.js
@@ -2,17 +2,14 @@ let gform = document.getElementById('gform');
 let captcha = document.getElementById('g-recaptcha-response');
 
 gform.addEventListener('submit', function (event) {
-    event.preventDefault();
-
     var response = grecaptcha.getResponse();
 
     if (response.length === 0) {
+        event.preventDefault();
         alert("Please complete the captcha!");
 
         return;
     }
-
-    event.target.submit();
 
     alert("Success! Redirecting to guestbook page...");
     window.location.href = "../guestbook/";
