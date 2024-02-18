919f3e82d2bbe3312c2e0a21c19eed4e004264f4
skifli <121291719+skifli@users.noreply.github.com>
2024-02-18 18:18:29 +0000

    fix: sign guestbook pages clashing

--- assets/js/pages/guestbook.js
+++
@@ -1,9 +0,0 @@
-import { openNewPage } from "../global.js";
-
-let signGuestbook = document.getElementById('sign-guestbook');
-
-signGuestbook.addEventListener("click", function () {
-    let guestbook = document.createElement('div');
-
-    openNewPage(null, "../sign");
-});
